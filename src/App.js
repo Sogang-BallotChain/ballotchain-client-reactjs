1e5137ce5cb1732c8f9e4ab40a271b4814957144
omnipede <omnipede@naver.com>
2019-11-18 20:32:33 +0900

    Create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mainbody from './Main/Mainbody'
+
+import './App.css';
+
+function App() {
+  return (
+    <Mainbody />
+  );
+}
+
+export default App;
---
+++ src/Main/Mainbody.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Mainbody extends React.Component {
+
+    render(){
+        return(
+            <section id = "square">
+                
+            </section>
+        )
+    }
+}
+
+export default Mainbody
