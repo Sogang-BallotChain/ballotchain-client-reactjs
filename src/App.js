d4cbcbccfe8e52c84767aa9a817b20eb1f5b120d
omnipede <omnipede@naver.com>
2019-11-20 15:24:52 +0900

    로그인 & 로그아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,32 +1,90 @@
 import React from 'react';
 import Explorer from './Components/Explorer'
+import LoginModal from './Components/LoginModal'
 import logo_white from './assets/logo_white.png'
 
-import {Layout, Menu, Button} from 'antd'
+import {Layout, Menu, Button, notification, Icon} from 'antd'
 import 'antd/dist/antd.css';
 import './App.css';
 
 const {Header, Content, Footer} = Layout;
 
-function App() {
+class App extends React.Component {
+
+  state={
+    login_modal_visible: false,
+    user_login: false,
+    user_email: ""
+  }
+
+  onClickLoginBtn = (e) => {
+    this.setState({
+      login_modal_visible: true
+    })
+  }
+
+  onClickLoginCancel = (e) => {
+    this.setState({
+      login_modal_visible: false
+    })
+  }
+
+  onClickLogoutBtn = (e) => {
+    this.setState({
+      user_email: "",
+      user_login: false
+    })
+  }
+
+  onLoginSuccess = (e, user_email) => {
+    this.setState({
+      login_modal_visible: false,
+      user_login: true,
+      user_email: user_email
+    })
+  }
+
+  onLoginFailed = (e) => {
+    this.setState({
+      login_modal_visible: false
+    })
+
+    notification.open({
+      message: 'Login failed!',
+      description: '이메일 또는 비밀번호를 다시 확인해주세요.',
+      icon: <Icon type="exclamation" style={{ color: 'red' }} />
+    })
+  }
+
+  render () {
   return (
     <Layout className="layout" style={{minWidth: "720px"}}>
       {/* Header */}
       <Header>
         <div className="logo">
-          <img  src={logo_white } 
+          <img  src={logo_white} 
           style={{margin: "16px 24px 16px 0", position: "absolute", top: 0}}alt="fireSpot"/>
         </div>
 
         <div className="btns" style={{top: 0, float: "right"}}>
-          <Button size="large" style = {{margin: "8px 20px 0 0"}}> Signup </Button> 
-          <Button size="large" type="primary" style = {{margin: "8px"}}> Login </Button> 
+          {
+            this.state.user_login === false 
+            ?
+            <div>
+            <Button style = {{margin: "8px 20px 0 0"}}> Signup </Button> 
+            <Button type="primary" style = {{margin: "8px"}} onClick={this.onClickLoginBtn}> Login </Button> 
+            </div>
+            : 
+            <div>
+            <Button type="danger" style = {{margin: "8px 20px 0 0"}} onClick={this.onClickLogoutBtn}> Logout </Button> 
+            </div>
+          }
         </div>
 
         <Menu
           theme="dark"
           mode = "horizontal"
-          defaultSelectedKeys={['2']}
+          defaultSelectedKeys={['1']}
           style = {{lineHeight: '64px'}}
         >
           <Menu.Item key="1">About us </Menu.Item>
@@ -40,14 +98,22 @@ function App() {
         <div style={{background: '#fff', padding: 24, minHeight: 560}}>
           <Explorer />
         </div>
-      </Content>
 
+        <LoginModal 
+          visible={this.state.login_modal_visible} 
+          onCancel ={this.onClickLoginCancel} 
+          onLoginSuccess = {this.onLoginSuccess}
+          onLoginFailed = {this.onLoginFailed}
+        />
+
+      </Content> 
       {/* Footer */}
       <Footer style={{textAlign: 'center', minHeight: 280}}>
          BallotChain @2019 Created by omnipede
       </Footer>
     </Layout>
   );
+  }
 }
 
 export default App;
---
+++ src/Components/LoginModal.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import axios from 'axios'
+
+import {
+    Modal, Form, Input, Button, Layout, Icon
+} from 'antd'
+
+/* 
+    props
+    1) handleCancel (visibility)
+    2) onLoginSuccess
+    3) onLoginFailed
+*/
+
+const {
+    Header
+} = Layout;
+
+const LoginForm = Form.create({name: 'login'}) (
+
+    class extends React.Component {
+        render () {
+            const {getFieldDecorator} = this.props.form;
+            return (
+                <div>
+                    <Modal visible={this.props.visible} footer={null} onCancel={this.props.onCancel}>
+                        <Layout style={{ background: '#fff' }}>
+                            <Header style={{ background: '#fff', padding: 0 }} align="center">
+                                <h1> Log in </h1>
+                            </Header>
+                            <Form onSubmit = {this.props.onSubmit} className="login-form">
+                                <Form.Item>
+                                    {getFieldDecorator('username', {
+                                        rules: [{ required: true, message: 'Please input your email!' }],
+                                    })(
+                                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0, 0, 0, .25)' }} />} placeholder="Email" />
+                                    )}
+                                </Form.Item>
+                                <Form.Item>
+                                    {getFieldDecorator('password', {
+                                        rules: [{ required: true, message: 'Please input your passoword!' }],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                    )}
+                                </Form.Item>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" className="login-form-button" block>
+                                        Log in
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Layout>
+                    </Modal>
+                </div>
+            )
+        }
+    }
+);
+
+class LoginModal extends React.Component {
+
+    saveFormRef = (formRef) => {
+        this.formRef = formRef;
+    }
+
+    handleCacel = (e) => {
+        this.props.onCancel()
+        this.formRef.props.form.resetFields();
+    }
+
+    handleSubmit = (e) => {
+        const form = this.formRef.props.form;
+        e.preventDefault();
+        form.validateFields(async (err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                const res = await axios.post('/user/signin', {
+                    "email": values.username,
+                    "password": values.password
+                });
+                
+                if (res.status === 200) {
+                    if (res.data.success === 1) {
+                        console.log("Login success")
+                        this.props.onLoginSuccess(values.username)
+                    }
+                    else {
+                        console.log("Login error")
+                        this.props.onLoginFailed()
+                    }
+                }
+                else {
+                    console.log("Login error")
+                    this.props.onLoginFailed()
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <LoginForm
+                wrappedComponentRef = {this.saveFormRef}
+                visible = {this.props.visible}
+                onCancel = {this.handleCancel}
+                onSubmit = {this.handleSubmit}
+            />
+        )
+    }
+}
+
+export default LoginModal
